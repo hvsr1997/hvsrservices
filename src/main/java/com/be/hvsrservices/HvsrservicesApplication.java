e2c6e18bb4a9590ef00d875795acc8f24b4457cc
hvsr1997 <rathoreharshvardhansingh1997@gmail.com>
2022-12-21 22:50:18 +0530

    first commit

---
+++ src/main/java/com/be/hvsrservices/HvsrservicesApplication.java
@@ -0,0 +1,13 @@
+package com.be.hvsrservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HvsrservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HvsrservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/be/hvsrservices/HvsrservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.hvsrservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HvsrservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
